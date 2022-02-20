bebc81d12c8f3a2cfd811797c581f11ee12b3ca5
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-20 15:36:51 +0000

    add 2.3.2 - usage of 'When' in enums

---
+++ src/chapter02_basics/2.3.2_WhenEnums.kt
@@ -0,0 +1,16 @@
+package chapter02_basics
+
+fun getMnemonic(color: Color) =
+    when (color) {
+        Color.RED -> "Richard"
+        Color.ORANGE -> "Of"
+        Color.YELLOW -> "York"
+        Color.GREEN -> "Gave"
+        Color.BLUE -> "Battle"
+        Color.INDIGO -> "In"
+        Color.VIOLET -> "Vain"
+    }
+
+fun main() {
+    println(getMnemonic(Color.BLUE))
+}
