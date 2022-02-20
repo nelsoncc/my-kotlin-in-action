050ce7666c8af123ee7c413a6d863518d7b3c69e
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-20 15:46:16 +0000

    add 2.3.3 - Using When With Arbitrary Objects

---
+++ src/chapter02_basics/2.3.3_UsingWhenWithArbitraryObjects.kt
@@ -0,0 +1,16 @@
+package chapter02_basics
+
+import chapter02_basics.colors.Color
+import chapter02_basics.colors.Color.*
+
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)) {
+        setOf(RED, YELLOW) -> ORANGE
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty color")
+    }
+
+fun main() {
+    println(mix(BLUE, YELLOW))
+}
