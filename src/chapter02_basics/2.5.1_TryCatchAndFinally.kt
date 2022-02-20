0199811f3476bd3d5458fbaa5704c09e1af37e34
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-20 23:12:25 +0000

    add 2.5.1 - TryCatchAndFinally

---
+++ src/chapter02_basics/2.5.1_TryCatchAndFinally.kt
@@ -0,0 +1,20 @@
+package chapter02_basics
+
+import java.io.BufferedReader
+import java.io.StringReader
+
+fun readNumber(reader: BufferedReader): Int? {
+    try {
+        val line = reader.readLine()
+        return Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        return null
+    } finally {
+        reader.close()
+    }
+}
+
+fun main() {
+    val reader = BufferedReader(StringReader("239"))
+    println(readNumber(reader))
+}
