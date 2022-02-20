686027f3a17298fdd54743a353a0d8c671916f25
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-20 15:36:51 +0000

    add 2.3.2 - usage of 'When' in enums

---
+++ src/chapter02_basics/2.3.2_WhenEnums2.kt
@@ -0,0 +1,14 @@
+package chapter02_basics
+
+import chapter02_basics.colors.Color
+import chapter02_basics.colors.Color.*
+
+fun getWarmth(color: Color) = when(color) {
+    RED, ORANGE, YELLOW -> "warm"
+    GREEN -> "neutral"
+    BLUE, INDIGO, VIOLET -> "cold"
+}
+
+fun main(args: Array<String>) {
+    println(getWarmth(Color.ORANGE))
+}
---
+++ src/chapter02_basics/2.3.2_WhenEnumsCombined.kt
@@ -0,0 +1,12 @@
+package chapter02_basics
+
+fun getWarmth(color: Color) =
+    when (color) {
+        Color.RED, Color.ORANGE, Color.YELLOW -> "warm"
+        Color.GREEN -> "neutral"
+        Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
+    }
+
+fun main() {
+    println(getWarmth(Color.INDIGO))
+}
