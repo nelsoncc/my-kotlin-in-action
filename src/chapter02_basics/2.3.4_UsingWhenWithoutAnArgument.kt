7ae5b44c244b2fb1cbc140ee9d7e0b34085a26b9
Nelson Correia <nelson.cintra.c@gmail.com>
2022-02-20 15:53:32 +0000

    add 2.3.4 - Using when without an argument

---
+++ src/chapter02_basics/2.3.4_UsingWhenWithoutAnArgument.kt
@@ -0,0 +1,22 @@
+package chapter02_basics
+
+import chapter02_basics.colors.Color
+import chapter02_basics.colors.Color.*
+
+fun mixOptimized(c1: Color, c2: Color) =
+    when {
+        (c1 == RED && c2 == YELLOW) ||
+                (c1 == YELLOW && c2 == RED) ->
+            ORANGE
+        (c1 == YELLOW && c2 == BLUE) ||
+                (c1 == BLUE && c2 == YELLOW) ->
+            GREEN
+        (c1 == BLUE && c2 == VIOLET) ||
+                (c1 == VIOLET && c2 == BLUE) ->
+            INDIGO
+        else -> throw Exception("Dirty color")
+    }
+
+fun main() {
+    println(mixOptimized(BLUE, YELLOW))
+}
